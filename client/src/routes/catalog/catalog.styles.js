3d558f0a2fce18e35306a313229ee001426b9b84
suhobski <suhobski@mail.ru>
2022-06-05 00:09:24 +0300

    feat: add redirect

--- client/src/routes/catalog/catalog.styles.js
+++ client/src/routes/catalog/catalog.styles.js
@@ -29,6 +29,7 @@ export const PreviewShadow = styled.div`
   position: absolute;
   width: 100%;
   height: 100%;
+  transition: 0.3s;
 
   ${CategoryPreview}:hover & {
     background-color: rgba(0, 0, 0, 0.3);
